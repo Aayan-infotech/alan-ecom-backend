010d078518b9149d85b1f0c97335cbe4682dbc57
abhi5ek <abhishekpandey7676@gmail.com>
2024-10-24 11:35:06 +0530

    delete Category

--- controllers/CategoryControllers.js
+++ controllers/CategoryControllers.js
@@ -1,39 +1,72 @@
 
 const Category = require('../models/CategoryModel');
 
-const createCategory = async (req, res) => {
+const createSubCategory = async (req, res) => {
   try {
     const { categoryName, subcategoryName } = req.body;
 
     if (!categoryName || !subcategoryName) {
-        return res.status(400).json({ message: "Category name and subcategory name are required." });
+      return res.status(400).json({ message: "Category name and subcategory name are required." });
     }
 
     if (!['Doors', 'Windows'].includes(categoryName)) {
-        return res.status(400).json({ message: 'Category name must be either "Doors" or "Windows".' });
+      return res.status(400).json({ message: 'Category name must be either "Doors" or "Windows".' });
     }
 
     let category = await Category.findOne({ categoryName });
 
     if (category) {
-        category.subcategories.push({
-            subcategoryName
-        });
+      category.subcategories.push({
+        subcategoryName
+      });
     } else {
-        category = new Category({
-            categoryName,
-            subcategories: [{
-                subcategoryName
-            }],
-        });
+      category = new Category({
+        categoryName,
+        subcategories: [{
+          subcategoryName
+        }],
+      });
     }
 
     const savedCategory = await category.save();
-    return res.status(201).json(savedCategory); 
-} catch (error) {
+    return res.status(201).json(savedCategory);
+  } catch (error) {
     console.error('Error adding/updating category:', error);
     return res.status(500).json({ message: 'Error adding/updating category', error });
-}
+  }
+};
+
+const addSubSubcategory = async (req, res) => {
+  try {
+    const { categoryName, subcategoryName, subSubcategoryName } = req.body;
+
+    if (!['Doors', 'Windows'].includes(categoryName)) {
+      return res.status(400).json({ message: 'Category name must be either "Doors" or "Windows".' });
+    }
+
+    let category = await Category.findOne({ categoryName });
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    let subcategory = category.subcategories.find(sub => sub.subcategoryName === subcategoryName);
+    if (!subcategory) {
+      return res.status(404).json({ message: 'Subcategory not found' });
+    }
+
+    if (subSubcategoryName && subSubcategoryName.trim() !== '') {
+      subcategory.subSubcategories.push({ subSubcategoryName });
+    } else {
+      return res.status(400).json({ message: 'Sub-subcategory name cannot be empty.' });
+    }
+
+    const updatedCategory = await category.save();
+
+    res.status(200).json({ message: 'Sub-Subcategory added successfully', updatedCategory });
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding sub-subcategory', error });
+  }
 };
 
 
@@ -43,6 +76,71 @@ const getAllCategories = async (req, res) => {
     res.status(200).json(categories);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteSubCategory = async (req, res) => {
+  try {
+    const { id, categoryName } = req.params;
+    const category = await Category.findOne({ categoryName: categoryName });
+
+    if (!category)
+      return res.status(404).json({ message: 'Category not found' });
+
+    category.subcategories = category.subcategories.filter(subcategory => subcategory._id.toString() !== id);
+
+    await category.save();
+
+    res.status(200).json({ message: 'Subcategory deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteSubSubCategory = async (req, res) => {
+  try {
+    const { id, categoryName, subSubcategoryName } = req.params;
+
+    const category = await Category.findOne({ categoryName });
+
+    if (!category) {
+      return res.status(404).json({
+        message: 'Category not found',
+      });
+    }
+
+    const subcategory = category.subcategories.find(
+      (subcat) => subcat.subcategoryName === subSubcategoryName
+    );
+
+    if (!subcategory) {
+      return res.status(404).json({
+        message: 'Subcategory not found',
+      });
+    }
+
+    const subSubcategoryIndex = subcategory.subSubcategories.findIndex(
+      (subSubcat) => subSubcat._id.toString() === id
+    );
+
+    if (subSubcategoryIndex === -1) {
+      return res.status(404).json({
+        message: 'Sub-subcategory not found',
+      });
+    }
+
+    subcategory.subSubcategories.splice(subSubcategoryIndex, 1);
+
+    await category.save();
+
+    res.status(200).json({
+      message: 'Sub-subcategory deleted successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error occurred',
+      error: error.message,
+    });
   }
 };
 
@@ -74,74 +172,12 @@ const updateCategory = async (req, res) => {
   }
 };
 
-
-const deleteCategory = async (req, res) => {
-  try {
-    const category = await Category.findByIdAndDelete(req.params.id);
-    if (!category) return res.status(404).json({ message: 'Category not found' });
-    res.status(204).json({ message: 'Category deleted' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-
-const addSubcategory = async (req, res) => {
-  const { subcategory } = req.body;
-
-  try {
-    const category = await Category.findById(req.params.id);
-    if (!category) return res.status(404).json({ message: 'Category not found' });
-
-    category.subcategories.push(subcategory);
-    await category.save();
-
-    res.status(200).json(category);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-
-const addSubSubcategory = async (req, res) => {
-  try {
-    const { categoryName, subcategoryName, subSubcategoryName } = req.body;
-
-    if (!['Doors', 'Windows'].includes(categoryName)) {
-      return res.status(400).json({ message: 'Category name must be either "Doors" or "Windows".' });
-    }
-
-    let category = await Category.findOne({ categoryName });
-
-    if (!category) {
-      return res.status(404).json({ message: 'Category not found' });
-    }
-
-    let subcategory = category.subcategories.find(sub => sub.subcategoryName === subcategoryName);
-      if (!subcategory) {
-      return res.status(404).json({ message: 'Subcategory not found' });
-    }
-
-    if (subSubcategoryName && subSubcategoryName.trim() !== '') {
-      subcategory.subSubcategories.push({ subSubcategoryName });
-    } else {
-      return res.status(400).json({ message: 'Sub-subcategory name cannot be empty.' });
-    }
-
-    const updatedCategory = await category.save();
-
-    res.status(200).json({ message: 'Sub-Subcategory added successfully', updatedCategory });
-  } catch (error) {
-    res.status(500).json({ message: 'Error adding sub-subcategory', error });
-  }
-};
-
 module.exports = {
-    createCategory,
-    getAllCategories,
-    getCategoryById,
-    updateCategory,
-    deleteCategory,
-    addSubcategory,
-    addSubSubcategory
+  createSubCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategory,
+  deleteSubCategory,
+  addSubSubcategory,
+  deleteSubSubCategory
 }
--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createCategory, getAllCategories, addSubSubcategory } = require('../controllers/CategoryControllers');
+const { getAllCategories, createSubCategory, deleteSubCategory, addSubSubcategory, deleteSubSubCategory } = require('../controllers/CategoryControllers');
 
 const router = express.Router();
 
-router.post('/addsubcategory', createCategory);
+router.post('/addsubcategory', createSubCategory);
 router.post('/addsubsubcategory', addSubSubcategory);
 router.get('/', getAllCategories);
+router.delete('/delete-subcategory/:categoryName/:id', deleteSubCategory);
+router.delete('/delete-subsubcategory/:categoryName/:subSubcategoryName/:id', deleteSubSubCategory);
 
 module.exports = router;
