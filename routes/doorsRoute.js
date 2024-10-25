4265bfdce704a39d1bab47d45a00d121d68afb64
abhi5ek <abhishekpandey7676@gmail.com>
2024-10-25 16:59:14 +0530

    update door

--- routes/doorsRoute.js
+++ routes/doorsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {createDoorss, allDoorsProduct, deleteDoorsProduct } = require('../controllers/doorsProductController');
+const {createDoorss, allDoorsProduct, deleteDoorsProduct, updateDoorsProduct} = require('../controllers/doorsProductController');
 
 const router = express.Router();
 
 router.post('/add-doors', createDoorss);
 router.get('/', allDoorsProduct);
 router.delete('/delete-door/:id', deleteDoorsProduct);
+router.put('/update-door/:id', updateDoorsProduct);
 
 module.exports = router;
