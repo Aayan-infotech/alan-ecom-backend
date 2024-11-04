f98643501e02c8798a9c416e3e06406f3202e35f
abhi5ek <abhishekpandey7676@gmail.com>
2024-11-04 14:49:29 +0530

    send email

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,7 +3,7 @@ const createError = require("http-errors");
 
 const sendEmail = async (req, res, next) => {
     try {
-        const { email, orderId, fullName, country, address, landmark, quantity, productId } = req.body;
+        const { email, orderId, firstName, lastName, country, address, landmark, quantity, productId } = req.body;
 
         const mailTransporter = nodemailer.createTransport({
             service: "gmail",
@@ -17,7 +17,7 @@ const sendEmail = async (req, res, next) => {
             from: "abhishekpandey7676@gmail.com",
             subject: "Order Confirmation",
             to: email,
-            text: `Hello ${fullName},
+            text: `Hello ${firstName} ${lastName},
 
             We are pleased to confirm your order with the following details:
             Order ID: ${orderId}
@@ -25,14 +25,24 @@ const sendEmail = async (req, res, next) => {
             Quantity: ${quantity}
             Shipping Address: ${address}, ${landmark}, ${country}
 
+            Your account details:
+            Username: ${email}
+            Password: ${firstName}
+
             Thank you for shopping with us!`,
             html: `
         <html>
         <head><title>Order Confirmation</title></head>
         <body>
           <h1>Order Confirmation</h1>
-          <p>Dear ${fullName},</p>
-          <p>Thank you for your order. Here are the details:</p>
+          <p>Dear ${firstName} ${lastName},</p>
+          <p>Thank you for your order. </p>
+          <p>Your account details:</p>
+          <ul>
+            <li><b>Username:</b> ${email}</li>
+            <li><b>Password:</b> ${firstName}</li>
+          </ul>
+          <p>Here are the details:</p>
           <ul>
             <li><b>Order ID:</b> ${orderId}</li>
             <li><b>Product ID:</b> ${productId}</li>
