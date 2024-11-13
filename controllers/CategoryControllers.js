38f0d7332d463f6fded20b1621b6cb17ca7a0bcc
abhi5ek <abhishekpandey7676@gmail.com>
2024-11-13 15:05:50 +0530

    category

--- controllers/CategoryControllers.js
+++ controllers/CategoryControllers.js
@@ -146,7 +146,11 @@ const addSubSubcategory = async (req, res) => {
 const getAllCategories = async (req, res) => {
   try {
     const categories = await Category.find();
-    res.status(200).json(categories);
+    res.status(200).json({
+      status : 200,
+      message: "All Category",
+      data: categories
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
